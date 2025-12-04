987a3cb05dce7ce1680b8e3eb8e1163f3757286f
svatal <swata@centrum.cz>
2025-12-04 22:53:52 +0100

    04 cleaned up

--- src/04.ts
+++ src/04.ts
@@ -3,13 +3,11 @@ import { input } from "./04-input";
 import { IPosition, neighbors8, valueInMap } from "./utils/position2D";
 
 export function doIt(progress: (...params: any[]) => void) {
-  const parsed = input.split(`\n`).map(
-    (line) => line.split("") //
-  );
-  const first = parsed.reduce((acc, curr, currIdx) => acc + curr.filter((cell, cellIdx) => canBeRemoved({ x: cellIdx, y: currIdx, symbol: cell })).length, 0);
+  const parsed = input.split(`\n`).map((line) => line.split(""));
+  const first = getRemovableCells().length;
   let second = 0;
   do {
-    const toRemove = parsed.reduce((acc, curr, currIdx) => [...acc, ...curr.map((cell, cellIdx) => ({ x: cellIdx, y: currIdx, symbol: cell })).filter(canBeRemoved)], [] as IPosition[]);
+    const toRemove = getRemovableCells();
     if (toRemove.length === 0) {
       break;
     }
@@ -20,6 +18,10 @@ export function doIt(progress: (...params: any[]) => void) {
   } while (true);
 
   console.log(first, second);
+
+  function getRemovableCells(): IPosition[] {
+    return parsed.reduce((acc, curr, currIdx) => [...acc, ...curr.map((symbol, cellIdx) => ({ x: cellIdx, y: currIdx, symbol })).filter(canBeRemoved)], [] as IPosition[]);
+  }
 
   function canBeRemoved(cell: IPosition & { symbol: string | undefined }): boolean {
     return cell.symbol === "@" && neighbors8(cell).filter((pos) => valueInMap(parsed)(pos) === "@").length < 4;
